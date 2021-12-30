415f35fd81f3228bd5b57e854fe50e60db2bbbed
Kristjan <krix@planet.ee>
2021-12-30 20:31:28 +0200

    added isLoading state and refactored reducers

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -4,15 +4,19 @@ import {
   CREATE,
   UPDATE,
   DELETE,
+  START_LOADING,
+  END_LOADING,
 } from '../constants/actionTypes';
 import * as api from '../api';
 
 //Action creators
 export const getPosts = (page) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
     const { data } = await api.fetchPosts(page);
 
     dispatch({ type: FETCH_ALL, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error);
   }
@@ -21,11 +25,14 @@ export const getPosts = (page) => async (dispatch) => {
 //get posts by search
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
+
     const {
       data: { data },
     } = await api.fetchPostsBySearch(searchQuery);
 
     dispatch({ type: FETCH_BY_SEARCH, payload: data });
+    dispatch({ type: END_LOADING });
   } catch (error) {
     console.log(error);
   }
@@ -33,6 +40,8 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
   try {
+    dispatch({ type: START_LOADING });
+
     const { data } = await api.createPost(post);
 
     console.log(data);
